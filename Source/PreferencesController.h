9251cab5d32c28f59f9a78cca4d101d31fe8d549
npeelman <npeelman@ca9e0de3-3d06-0410-8c0f-843a1a646755>
2009-05-05 17:56:07 +0000

    Fixed the glitchy resizing (I think).  I started to add the framework for a third, toggle-able preference pane for Advanced settings.  Was having issues with it putting the Advanced button as the leading toolbar item, regardless of where I told it to put it.
    There has been major cleanup done to the preferences controller.   The code is almost tolerable to look at now.  I will be doing more cleanup and intelligent commenting in the near future.

--- Source/PreferencesController.h
+++ Source/PreferencesController.h
@@ -5,18 +5,21 @@
 @interface PreferencesController : NSObject {
     IBOutlet NSView *connectionView;
     IBOutlet NSView *generalView;
+	IBOutlet NSView *advancedView;
     IBOutlet NSWindow *preferencesWindow;
-    IBOutlet NSTabView *tabView;
-	IBOutlet NSBox *generalBox;
 	IBOutlet NSBox *sessionBox;
 	IBOutlet NSBox *devicesBox;
 	IBOutlet NSBox *performanceBox;	
-	
+	IBOutlet NSUserDefaultsController *defaultsController;
+	IBOutlet NSButton *showAdvancedCheckbox;
+
+	NSUserDefaults *userDefaults;
 	NSToolbar *prefsToolbar;
 	NSMutableDictionary *toolbarItems;
 }
 
 -(void) mapTabsToToolbar;
 -(IBAction)	changePanes: (id)sender;
+-(IBAction) toggleAdvanced: (id)sender;
 
 @end
--- Source/PreferencesController.m
+++ Source/PreferencesController.m
@@ -26,50 +26,38 @@
 
 	[[preferencesWindow contentView] setWantsLayer:YES];
 	[generalView setWantsLayer:YES];
-	[connectionView setWantsLayer:YES];
-	
-	ZNLog(@"Init General View: %@", generalView);
+	[connectionView setWantsLayer:YES];	
+	[advancedView setWantsLayer:YES];
+//	ZNLog(@"Init General View: %@", generalView);
 	[self changePanes:nil];
 }
 
 -(void) mapTabsToToolbar
 {
-    // Create a new toolbar instance, and attach it to our document window 
-	prefsToolbar = [[tabView window] toolbar];
-	int				itemCount = 0,
-	x = 0;
-	NSTabViewItem	*currPage = nil;
+	//Define Toolbar Items
+	[toolbarItems removeAllObjects];
+	[toolbarItems setObject:@"General" forKey:@"General"];
+	[toolbarItems setObject:@"Connections" forKey:@"Connections"];
+//	[toolbarItems setObject:@"Advanced" forKey:@"Advanced"];
 	
-	if( prefsToolbar == nil )   // No toolbar yet? Create one!
+    // Create a new toolbar instance, and attach it to our document window 
+	prefsToolbar = [preferencesWindow toolbar];
+	int				itemCount = 0, x = 0;	
+	
+	if( prefsToolbar == nil )
+	{
 		prefsToolbar = [[NSToolbar alloc] initWithIdentifier: @"CoRDPrefsToolbar"];
-    // Set up toolbar properties: Allow customization, give a default display mode, and remember state in user defaults 
+	}
+
     [prefsToolbar setAllowsUserCustomization: NO];
     [prefsToolbar setAutosavesConfiguration: NO];
     [prefsToolbar setDisplayMode: NSToolbarDisplayModeIconAndLabel];
-	
-	// Set up item list based on Tab View:
-	itemCount = [tabView numberOfTabViewItems];
-
-	[toolbarItems removeAllObjects];	// In case we already had a toolbar.
-	
-	[toolbarItems setObject:@"General" forKey:@"General"];
-	[toolbarItems setObject:@"Connections" forKey:@"Connections"];
-    
-	NSLog(@"Toolbar Items Count:  %i", [toolbarItems count]);
-    // We are the delegate
     [prefsToolbar setDelegate: self];
-
 
     // Attach the toolbar to the document window 
     [preferencesWindow setToolbar: prefsToolbar];
 	[preferencesWindow setShowsToolbarButton:NO];
-	// Set up window title:
-	currPage = [tabView selectedTabViewItem];
-	if( currPage == nil )
-		currPage = [tabView tabViewItemAtIndex:0];
-	
-	if( [prefsToolbar respondsToSelector: @selector(setSelectedItemIdentifier:)] )
-		[prefsToolbar setSelectedItemIdentifier: [currPage identifier]];
+
 }
 
 -(IBAction)changePanes:(id)sender
@@ -84,8 +72,12 @@
 	else if ([[sender label] isEqualToString:@"Connections"])
 	{
 		newPane = connectionView;
+	}
+	else if ([[sender label] isEqualToString:@"Advanced"])
+	{
+		newPane = advancedView;
 	} else {
-		ZNLog(@"No Sender, Selecting General");
+		//ZNLog(@"No Sender, Selecting General");
 		newPane = generalView;
 	}
 	
@@ -100,29 +92,31 @@
 	[prefsToolbar setSelectedItemIdentifier:([sender label]) ? [sender label] : @"General"];
 
 	
-	ZNLog(@"newPane: %@", newPane);
-	// Preserve upper left point of window during resize.
+	//ZNLog(@"newPane: %@", newPane);
 	NSRect newFrame = [preferencesWindow frame];
-	ZNLog(@"Original preferences window frame: %@", NSStringFromRect(newFrame));
-	
+	//ZNLog(@"Original preferences window frame: %@", NSStringFromRect(newFrame));
 	newFrame.size.height = [newPane frame].size.height + ([preferencesWindow frame].size.height - [[preferencesWindow contentView] frame].size.height);
 	newFrame.size.width = [newPane frame].size.width; 
 	newFrame.origin.y += ([[preferencesWindow contentView] frame].size.height - [newPane frame].size.height);
 	
-	ZNLog(@"Using preferences window frame: %@", NSStringFromRect(newFrame));
-	
+	//ZNLog(@"Using preferences window frame: %@", NSStringFromRect(newFrame));
+	[NSAnimationContext beginGrouping];
 	[[preferencesWindow animator] setFrame:newFrame display:YES];
 	[[preferencesWindow animator] setContentView:newPane];
+	[NSAnimationContext endGrouping];
 	
-	// Set appropriate resizing on window.
 	NSSize theSize = [preferencesWindow frame].size;
-	ZNLog(@"Original preferences window size: %@", NSStringFromSize(theSize));
+	//ZNLog(@"Original preferences window size: %@", NSStringFromSize(theSize));
 	theSize.height -= 100;
-	ZNLog(@"Using preferences window size: %@", NSStringFromSize(theSize));
+	//ZNLog(@"Using preferences window size: %@", NSStringFromSize(theSize));
 	
 	[preferencesWindow setMinSize:theSize];
 }
 
+-(IBAction) toggleAdvanced: (id)sender
+{
+
+}
 
 
 /* -----------------------------------------------------------------------------
@@ -133,37 +127,34 @@
 
 -(NSToolbarItem *) toolbar: (NSToolbar *)toolbar itemForItemIdentifier: (NSString *)itemIdent willBeInsertedIntoToolbar:(BOOL) willBeInserted
 {
-	NSLog(@"Creating Toolbar Item With Ident: %@", itemIdent);
-    // Required delegate method:  Given an item identifier, this method returns an item 
-    // The toolbar will use this method to obtain toolbar items that can be displayed in the customization sheet, or in the toolbar itself 
-    NSToolbarItem   *toolbarItem = [[[NSToolbarItem alloc] initWithItemIdentifier: itemIdent] autorelease];
+	NSToolbarItem   *toolbarItem = [[[NSToolbarItem alloc] initWithItemIdentifier: itemIdent] autorelease];
     NSString*		itemLabel;
-	NSLog(@"Toolbar Item Created, with Ident:  %@, %@", [toolbarItem itemIdentifier], [toolbarItems objectForKey:itemIdent]);
-    if( (itemLabel = [toolbarItems objectForKey:itemIdent]) != nil )
+
+    if ( (itemLabel = [toolbarItems objectForKey:itemIdent]) != nil )
 	{
-		// Set the text label to be displayed in the toolbar and customization palette 
 		[toolbarItem setLabel: itemLabel];
 		[toolbarItem setPaletteLabel: itemLabel];
-		[toolbarItem setTag:[tabView indexOfTabViewItemWithIdentifier:itemIdent]];
-		NSLog(@"Set Item Properties, Label %@, Palette Label %@, and Tag %i",[toolbarItem label],[toolbarItem paletteLabel], [toolbarItem tag]);
-		// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties 
+
+		// These aren't localized (yet)
 		[toolbarItem setToolTip: itemLabel];
 		if ([[toolbarItem label] isEqualToString:@"General"])
 		{
 			[toolbarItem setImage: [NSImage imageNamed:NSImageNamePreferencesGeneral]];
-		} else if ([[toolbarItem label] isEqualToString:@"Connections"])
+		} 
+		else if ([[toolbarItem label] isEqualToString:@"Connections"])
 		{
 			[toolbarItem setImage:[NSImage imageNamed:@"Windowed.png"]];
+		} 
+		else if ([[toolbarItem label] isEqualToString:@"Advanced"])
+		{
+			[toolbarItem setImage:[NSImage imageNamed:NSImageNameAdvanced]];;
 		}
 		
-		// Tell the item what message to send when it is clicked 
 		[toolbarItem setTarget: self];
 		[toolbarItem setAction: @selector(changePanes:)];
     }
 	else
 	{
-		// itemIdent refered to a toolbar item that is not provide or supported by us or cocoa 
-		// Returning nil will inform the toolbar this kind of item is not supported 
 		toolbarItem = nil;
     }
 	
@@ -173,9 +164,10 @@
 
 -(NSArray*) toolbarAllowedItemIdentifiers: (NSToolbar *) toolbar
 {
-	NSLog(@"Checking Allowed Idents");
-    NSMutableArray*	allowedItems = [[[toolbarItems allKeys] mutableCopy] autorelease];
+	//NSLog(@"Checking Allowed Idents");
 	
+    NSMutableArray*	allowedItems = [[[toolbarItems allKeys] mutableCopy] autorelease];
+
 	[allowedItems addObjectsFromArray: [NSArray arrayWithObjects: NSToolbarSeparatorItemIdentifier,
 										NSToolbarSpaceItemIdentifier, NSToolbarFlexibleSpaceItemIdentifier,
 										NSToolbarCustomizeToolbarItemIdentifier, nil] ];
@@ -184,30 +176,16 @@
 
 -(NSArray*) toolbarDefaultItemIdentifiers: (NSToolbar *) toolbar
 {
-	NSLog(@"Checking Default Idents");
-//	int					itemCount = [tabView numberOfTabViewItems],
-//	x;
-//	NSTabViewItem*		theItem = [tabView tabViewItemAtIndex:0];
-//	//NSMutableArray*	defaultItems = [NSMutableArray arrayWithObjects: [theItem identifier], NSToolbarSeparatorItemIdentifier, nil];
+	//NSLog(@"Checking Default Idents");
 	NSMutableArray* defaultItems = [NSMutableArray arrayWithArray:[toolbarItems allKeys]];
-	//
-//	for( x = 0; x < itemCount; x++ )
-//	{
-//		theItem = [tabView tabViewItemAtIndex:x];
-//		
-//		[defaultItems addObject: [theItem identifier]];
-//		NSLog(@"Adding Default Item: %@", [theItem identifier]); 
-//	}
-	
 	return defaultItems;
 }
 
 -(NSArray*) toolbarSelectableItemIdentifiers: (NSToolbar*)toolbar
 {
-	NSLog(@"Checking Selectable Item Idents");
+	//NSLog(@"Checking Selectable Item Idents");
 	return [toolbarItems allKeys];
 }
-
 
 - (void)windowWillClose:(NSNotification *)notification
 {
@@ -217,6 +195,7 @@
 - (void)windowDidBecomeKey:(NSNotification *)notification
 {
 	//Set Cmd-W to close the current window and not hte current session
+	
 	
 }
 
