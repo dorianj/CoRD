a6c3a1472db9cffada8d0bc16f1eab5c72426da7
npeelman <npeelman@ca9e0de3-3d06-0410-8c0f-843a1a646755>
2010-02-15 14:46:11 +0000

    Fixing Copyright on CRDServerGroup

--- trunk/Source/CRDServerGroup.h
+++ trunk/Source/CRDServerGroup.h
@@ -14,6 +14,7 @@
 	CoRD; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
 	Fifth Floor, Boston, MA 02110-1301 USA
 */
+
 #import <Cocoa/Cocoa.h>
 
 
--- trunk/Source/CRDServerGroup.m
+++ trunk/Source/CRDServerGroup.m
@@ -1,10 +1,19 @@
-//
-//  CRDServerGroup.m
-//  Cord
-//
-//  Created by Nick Peelman on 2/14/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+/*	Copyright (c) 2010 Nick Peelman <nick@peelman.us>
+ 
+ This file is part of CoRD.
+ CoRD is free software; you can redistribute it and/or modify it under the
+ terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later
+ version.
+ 
+ CoRD is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along with
+ CoRD; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #import "CRDServerGroup.h"
 
